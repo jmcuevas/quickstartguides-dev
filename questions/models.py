a36d4e58f23cc88388d5dfde81790bcd7a106f13
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-09-15 22:19:11 -0700

    Create initial model files (Question, Answer, Vote, Bookmark)

--- questions/models.py
+++ questions/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length = 255)
+    body = models.TextField()
+    total_votes = models.IntegerField(default=0)
+    # created_by = 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Answer(models.Model):
+    body = models.TextField()
+    # created_by = 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Vote(models.Model):
+    # user = 
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='votes')
+    upvote = models.BooleanField()
+
+class Bookmark(models.Model):
+    # user = 
+    question = models.ForeignKey('Question' on_delete=models.CASCADE, related_name='bookmarks')
+
