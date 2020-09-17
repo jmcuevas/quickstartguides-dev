4f07d77bc4345e9b0036bdfe955cc693af9e3f46
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-09-16 22:29:14 -0700

    Add relationships to User model. Question, Answer, Vote & Bookmark

--- questions/models.py
+++ questions/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users import models as user_models
 
 # Create your models here.
 
@@ -6,20 +7,21 @@ class Question(models.Model):
     title = models.CharField(max_length = 255)
     body = models.TextField()
     total_votes = models.IntegerField(default=0)
-    # created_by = models.ForeignKey()
+    created_by = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="questions")
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Answer(models.Model):
     body = models.TextField()
-    # created_by = 
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name="answers")
+    created_by = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="answers")
     created_at = models.DateTimeField(auto_now_add=True)
 
 class Vote(models.Model):
-    # user = 
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="votes")
     question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='votes')
     upvote = models.BooleanField()
 
 class Bookmark(models.Model):
-    # user = 
+    user = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="bookmarks")
     question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='bookmarks')
 
