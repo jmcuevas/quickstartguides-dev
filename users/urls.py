0ff21425a96c31f0c0b891a2988c6609d546e055
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-09-16 22:46:32 -0700

    Initial authentication setup Create views: login, logout & register Create initial templates: loging, register & layout Setup urls for login, logout & register

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.register, name="logout"),
+    path("register/", views.register, name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -3,4 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "users/index.html")
+    return render(request, "users/layout.html")
+
+def login(request):
+    return render(request, "users/login.html")
+
+def register(request):
+    return render(request, "users/register.html")
+
+def logout(request):
+    # Pending
+    pass
