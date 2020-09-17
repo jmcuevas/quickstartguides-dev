708ba19c49aa816f58673f1ad6291300b79b2b8c
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-09-16 22:28:12 -0700

    Register Answer, Bookmark, Question, Vote & User to Admin UI

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Answer, Bookmark, Question, Vote
 
-# Register your models here.
+admin.site.register(Answer)
+admin.site.register(Bookmark)
+admin.site.register(Question)
+admin.site.register(Vote)
