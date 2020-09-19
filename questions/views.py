56503babba0804137ae3822f400c2c3eab56f763
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-09-19 13:13:57 -0700

    Feature: New question complete Created template new.html Created new view Created new URL

--- questions/views.py
+++ questions/views.py
@@ -1,6 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+from django import forms
+from .models import Question
+
+# ----- Forms -----
+
+class NewQuestionForm(forms.Form):
+    title = forms.CharField(label="Title")
+    body = forms.CharField(label="Body", widget=forms.Textarea(attrs= {'class':'form-control', 'placeholder':'Write a question...'}))
+
+
+# ----- Views -----
 
 def index(request):
     return render(request, "questions/index.html")
+
+def new(request):
+
+    # Submit new Question Form
+    if request.method == "POST":
+        form = NewQuestionForm(request.POST)
+        
+        # Valid form data
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            body = form.cleaned_data["body"]
+            total_votes = 0
+            created_by = request.user
+
+            # Create and save question
+            new_question = Question(
+                title=title, 
+                body=body, 
+                total_votes=total_votes, 
+                created_by=created_by)
+            
+            new_question.save()
+            print("Sucess: Question saved")
+
+            return(HttpResponseRedirect(reverse("index")))
+
+        # Form not vaild
+        else:
+            print("Error: Post not Created")
+            return HttpResponseRedirect(reverse("new"))
+
+    # Get request to questions/new
+    else:
+        return render(request, 'questions/new.html', {"new_question_form": NewQuestionForm()})
