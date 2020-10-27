a1ff7ca9ab678eafabd6cc024682b2a6a5ceb609
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-10-26 22:20:54 -0700

    Add Guide model and migratef

--- guides/models.py
+++ guides/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from users import models as user_models
+from taggit.managers import TaggableManager
 
 # Create your models here.
+
+class Guide(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    slug = models.SlugField(unique=True, max_length=100)
+    tags = TaggableManager()
+    division = models.CharField(max_length=100)
+    created_by = models.ForeignKey(user_models.User, on_delete=models.CASCADE, related_name="guides")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
