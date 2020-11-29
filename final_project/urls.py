de9415550de67eeff21704ff22f6b077a225217c
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-11-29 09:49:25 -0800

    upldate urls.py to include settings

--- final_project/urls.py
+++ final_project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 from guides import views as guides_views
-from django.conf.urls.static import static
 
 
 urlpatterns = [
