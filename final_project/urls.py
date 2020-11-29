2c2d052cba5ca253b4c1f9cd64ce5dcccf350d93
Jose Manuel Cuevas <jc@MacBook-Pro-de-Jose.local>
2020-11-29 15:10:20 -0800

    Updates to handle image upload in production

--- final_project/urls.py
+++ final_project/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path("guides/", include("guides.urls")),
     path("ckeditor/", include("ckeditor_uploader.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
